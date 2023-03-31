e7c154e4bdcf41ed6f798aba2741b4a8bfbf35d7
kyli <ldh71777@gmail.com>
2023-03-31 17:44:47 +0800

    feat: monaco-editor

--- rspack.config.js
+++ rspack.config.js
@@ -1,24 +1,40 @@
+const path = require("path");
+
 /**
  * @type {import('@rspack/cli').Configuration}
  */
 module.exports = {
-	context: __dirname,
-	entry: {
-		main: "./src/main.jsx"
-	},
-	builtins: {
-		html: [
-			{
-				template: "./index.html"
-			}
-		]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svg$/,
-				type: "asset"
-			}
-		]
-	}
+  context: __dirname,
+  entry: {
+    app: "./src/index.js",
+    "editor.worker": "monaco-editor/esm/vs/editor/editor.worker.js",
+    "json.worker": "monaco-editor/esm/vs/language/json/json.worker",
+    "css.worker": "monaco-editor/esm/vs/language/css/css.worker",
+    "html.worker": "monaco-editor/esm/vs/language/html/html.worker",
+    "ts.worker": "monaco-editor/esm/vs/language/typescript/ts.worker",
+  },
+  output: {
+    globalObject: "self",
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  builtins: {
+    html: [
+      {
+        template: "./index.html",
+      },
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: "asset",
+      },
+      {
+        test: /\.ttf$/,
+        type: "asset",
+      },
+    ],
+  },
 };
